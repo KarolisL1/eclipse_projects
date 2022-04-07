0fc200a261a24a39e6290a42f588527b1fb9c438
karolisliepa <Karolis.Liepa@gmail.com>
2022-04-07 17:22:18 -0400

    hello

--- SpringProjects/HelloHuman/src/main/java/com/codingdojo/HelloHumanController.java
+++ SpringProjects/HelloHuman/src/main/java/com/codingdojo/HelloHumanController.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloHumanController {
     @RequestMapping("/")
-    public String index(@RequestParam(value="q", required = false) String searchQuery) {
+    public String index(@RequestParam(value="name", required = false) String searchQuery,
+    		@RequestParam(value="last_name", required = false) String last) {
     	if(searchQuery == null) {
     		return "Hello human";
     	}
     	else {
-    		return "Hello: " + searchQuery;
+    		return "Hello: " + searchQuery + " " + last;
     	}
     }
 }
