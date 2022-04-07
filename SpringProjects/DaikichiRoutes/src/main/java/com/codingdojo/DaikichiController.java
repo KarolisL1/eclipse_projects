fc236a722e33305d4d54b2f17ec1111fe08fc166
karolisliepa <Karolis.Liepa@gmail.com>
2022-04-07 19:01:40 -0400

    path

--- SpringProjects/DaikichiRoutes/src/main/java/com/codingdojo/DaikichiController.java
+++ SpringProjects/DaikichiRoutes/src/main/java/com/codingdojo/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.codingdojo;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,18 @@ public class DaikichiController {
     @RequestMapping("/daikichi/tommorow")
     public String tommorow() {
             return "Tommorow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+    
+    @RequestMapping("/m/daikichi/travel/{city}")
+    public String showLesson(@PathVariable("city") String city){
+    	return "Congratulations! You will soon travel to " + city;
+    }
+    
+    @RequestMapping("/m/daikichi/lotto/{number}")
+    public String showLesson(@PathVariable("number") Integer number){
+    	if(number % 2 == 0) {
+    		return "You will take a grand journey in the near future, but be weary of tempting offers";
+    	}
+    	return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
     }
 }
